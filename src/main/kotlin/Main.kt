a373e811770da67d3868f5e1fd20489b455f877b
eeun1225 <eeun9108@gmail.com>
2025-11-24 15:18:51 +0900

    feat: 메인 애플리케이션 진입점 구현
    - 의존성 주입 설정
    - 컨트롤러 실행

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,42 @@
 package store
 
-fun main() {
+import store.domain.product.ProductInventory
+import store.domain.promotion.PromotionRepository
+import store.service.PaymentService
+import store.service.PromotionService
+import store.service.StoreService
+import store.util.FileReader
+import store.view.InputView
+import store.view.OutputView
+import store.controller.StoreController
 
+fun main() {
+    try {
+        val products = FileReader.readProducts()
+        val promotions = FileReader.readPromotions()
+
+        val productInventory = ProductInventory()
+        products.forEach { productInventory.addProduct(it) }
+
+        val promotionRepository = PromotionRepository()
+        promotions.forEach { promotionRepository.save(it) }
+
+        val inputView = InputView()
+        val outputView = OutputView()
+        val promotionService = PromotionService(promotionRepository)
+        val storeService = StoreService(productInventory, promotionService, inputView)
+        val paymentService = PaymentService()
+
+        val controller = StoreController(
+            storeService = storeService,
+            paymentService = paymentService,
+            inputView = inputView,
+            outputView = outputView
+        )
+
+        controller.run()
+
+    } catch (e: Exception) {
+        println("[ERROR] 애플리케이션 실행 중 오류가 발생했습니다: ${e.message}")
+    }
 }
