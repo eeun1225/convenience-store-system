c8842c1cc20ac200901e6c39732572f099d2fa30
eeun1225 <eeun9108@gmail.com>
2025-11-24 16:55:13 +0900

    feat: Product 메서드 추가

--- src/main/kotlin/domain/product/Product.kt
+++ src/main/kotlin/domain/product/Product.kt
@@ -9,4 +9,17 @@ data class Product(
     val category: ProductCategory = ProductCategory.fromProductName(name)
 ) {
     fun hasPromotion() = promotion != null
+
+    fun canPurchase(requestQuantity: Int): Boolean {
+        return quantity >= requestQuantity
+    }
+
+    fun decreaseQuantity(amount: Int): Product {
+        require(amount <= quantity) { "재고가 부족합니다" }
+        return copy(quantity = quantity - amount)
+    }
+
+    fun isInCategory(category: ProductCategory): Boolean {
+        return this.category == category
+    }
 }
